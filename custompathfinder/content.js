e8c173b1cb6b17e160ac3483ba03ed92e03dcf1e
Sai Punith Bonagiri <saipunithbonagiri@gmail.com>
2022-08-09 00:47:28 +0530

    UI related changes with minor functionality

--- custompathfinder/content.js
+++ custompathfinder/content.js
@@ -1,5 +1,4 @@
 // configure theRoom
-debugger;
 window.theRoom.configure({
   blockRedirection: true,
   createInspector: true,
@@ -30,77 +29,83 @@ window.theRoom.configure({
   },
   keydown: function (element, event) {
     console.log("Inside keydown function in content.js");
-    // navigator
-    //   .clipboard
-    //   .writeText(
-    //     getSelector(element)
-    //   )
-    //   .then(
-    //     function () {
-    //       alert('The unique CSS selector successfully copied to clipboard')
-    //     },
-    //     function (err) {
-    //       alert('The unique CSS selector could not be copied to clipboard')
-    //     }
-    //   )
-
     console.log("Element Selected"+getSelector(element));
     var swalTemplateElement=document.getElementById('SwalTemplate');
     var swalTemplateContentCloned=swalTemplateElement.content.firstElementChild.cloneNode(true);
     Swal.fire({
-        // html:templateContentCloned
         html:swalTemplateContentCloned,
         showCloseButton: true,
-        showCancelButton: true,
         allowOutsideClick: false,
-        allowEscapeKey: false
+        allowEscapeKey: false,
+        confirmButtonText: 'Save',
+        willOpen:function(element){
+          window.theRoom.stop(true);
+          var o9SplitViewElement=document.getElementById('__splitview__');
+          if(o9SplitViewElement){
+            o9SplitViewElement.style.position='fixed';
+          }
+        },
+        willClose:function(element){
+          var o9SplitViewElement=document.getElementById('__splitview__');
+          if(o9SplitViewElement){
+            o9SplitViewElement.style.position='';
+          }
+        },
+        preConfirm:function(){
+          //To get the element's input values and storing them before swal closes by confirm button
+        }
+    }).then(function(result){
+      window.theRoom.start();
     });
 
     // so far so good
     // stop inspection
-    window.theRoom.stop(true);
+    // window.theRoom.stop(true);
   }
 })
 
+var generateAndAppendTemplate = function () {
+    var swalTemplateElement = document.createElement('template');
+    swalTemplateElement.setAttribute('id', "SwalTemplate");
+    var MainDiv = document.createElement('div');
+    MainDiv.setAttribute('class', 'MainDiv');
+
+    var nameElement = document.createElement('input');
+    nameElement.setAttribute('class', 'nameInput swal2-input');
+    nameElement.setAttribute('placeholder', 'Enter a name');
+
+    var selectorInputElement = document.createElement('input');
+    selectorInputElement.setAttribute('class', 'selectorElementInput swal2-input');
+    selectorInputElement.setAttribute('placeholder', 'Optional element selector');
+
+    var contentInputElement = document.createElement('textarea');
+    contentInputElement.setAttribute('class', 'contentInput swal2-textarea');
+    contentInputElement.setAttribute('placeholder', 'Enter the text for tour');
+    contentInputElement.setAttribute('maxlength', '100');
+
+    var eventTypeInputElement = document.createElement('input');
+    eventTypeInputElement.setAttribute('class', 'eventTypeInput swal2-input');
+    eventTypeInputElement.setAttribute('placeholder', 'Event Type During tour');
+
+    MainDiv.appendChild(nameElement);
+    MainDiv.appendChild(selectorInputElement);
+    MainDiv.appendChild(contentInputElement);
+    MainDiv.appendChild(eventTypeInputElement);
+
+    swalTemplateElement.content.appendChild(MainDiv);
+
+    document.body.appendChild(swalTemplateElement);
+};
+
 // inspector element styles
-var linkElement = document.createElement('link')
-linkElement.setAttribute('rel', 'stylesheet')
-linkElement.setAttribute('type', 'text/css')
-linkElement.setAttribute('href', 'data:text/css;charset=UTF-8,' + encodeURIComponent('.inspector-element { position: absolute; pointer-events: none; border: 2px solid tomato; transition: all 200ms; background-color: rgba(180, 187, 105, 0.2); z-index:2147483644 }'))
-document.head.appendChild(linkElement)
+var linkElement = document.createElement('link');
+linkElement.setAttribute('rel', 'stylesheet');
+linkElement.setAttribute('type', 'text/css');
+linkElement.setAttribute('href', 'data:text/css;charset=UTF-8,' + encodeURIComponent('.inspector-element { position: absolute; pointer-events: none; border: 2px solid tomato; transition: all 200ms; background-color: rgba(180, 187, 105, 0.2); z-index:2147483644 }'));
+document.head.appendChild(linkElement);
 
-//Modal creation start
-var swalTemplateElement=document.createElement('template');
-swalTemplateElement.setAttribute('id',"SwalTemplate");
-var MainDiv=document.createElement('div');
-MainDiv.setAttribute('class','MainDiv');
-
-var nameElement=document.createElement('input');
-nameElement.setAttribute('class','nameInput swal2-input');
-nameElement.setAttribute('placeholder','Enter a name');
-
-var selectorInputElement=document.createElement('input');
-selectorInputElement.setAttribute('class','selectorElementInput swal2-input');
-selectorInputElement.setAttribute('placeholder','Optional element selector');
-
-var contentInputElement=document.createElement('textarea');
-contentInputElement.setAttribute('class','contentInput swal2-textarea');
-contentInputElement.setAttribute('placeholder','Enter the text for tour');
-contentInputElement.setAttribute('maxlength','100');
-
-var eventTypeInputElement=document.createElement('input');
-eventTypeInputElement.setAttribute('class','eventTypeInput swal2-input');
-eventTypeInputElement.setAttribute('placeholder','Event Type During tour');
-
-MainDiv.appendChild(nameElement);
-MainDiv.appendChild(selectorInputElement);
-MainDiv.appendChild(contentInputElement);
-MainDiv.appendChild(eventTypeInputElement);
-
-swalTemplateElement.content.appendChild(MainDiv);
-
-document.body.appendChild(swalTemplateElement);
-//Modal creation end
+//Creation of Modal Template Element and appending it to body
+generateAndAppendTemplate();
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   // the expected message has arrived
@@ -108,4 +113,4 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 
   // inspection has started
   window.theRoom.start()
-})
+});
--- custompathfinder/popup.js
+++ custompathfinder/popup.js
@@ -1,18 +1,64 @@
+var toggleElementFunction = function () {
+  $('.toggle').click(function (e) {
+    e.preventDefault();
+
+    var outerTourElement= e.target.parentElement.parentElement;
+    var innerElement=outerTourElement.querySelector('.inner');
+    if(innerElement.style.display==='none' ||innerElement.style.display===""){
+      e.target.style.backgroundColor='black';
+      e.target.style.color='white';
+      innerElement.style.display='block';
+    }else{
+      e.target.style.backgroundColor='white';
+      e.target.style.color='black';
+      innerElement.style.display='none';
+    }
+  });
+};
+
+
+var initiateAllEventListeners=function(){
+  toggleElementFunction();
+  var accordionListElement=document.querySelector('.AccordionList');
+  var formInputElement=document.querySelector('.TourForm');
+  var createTourButton=document.querySelector('.CreateTour');
+  var createTourParent=createTourButton.parentElement;
+  var createTourElementButton = document.querySelector('.CreateTour');
+  var cancelButton=document.querySelector('.buttonGroupForm .cancelButton');
+  var saveButton=document.querySelector('.buttonGroupForm .saveButton');
+  createTourElementButton.addEventListener('click',function(e){
+    accordionListElement.style.display='none';
+    createTourParent.style.display='none';
+    formInputElement.style.display='flex';
+  });
+
+  cancelButton.addEventListener('click',function(e){
+    accordionListElement.style.display='block';
+    createTourParent.style.display='block';
+    formInputElement.style.display='none';
+  });
+
+  // saveButton.addEventListener('click',function(e){
+  //   //tour_id,tour_name,tour_description,tourUrl
+  // });
+}
+
 document.addEventListener('DOMContentLoaded', function () {
   // get the cta button element
-  var selectElementButton = document.getElementById('selectElement')
+  initiateAllEventListeners();
+  
 
   // handle cta button click event
   // to be able to start inspection
-  selectElementButton.addEventListener('click', function () {
+  // selectElementButton.addEventListener('click', function () {
 
-    // send the message to start inspection
-    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {data: null})
-    })
+  //   // send the message to start inspection
+  //   chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+  //     chrome.tabs.sendMessage(tabs[0].id, {data: null})
+  //   })
 
-    // close the extension popup
-    window.close()
+  //   // close the extension popup
+  //   window.close()
 
-  }, false)
+  // }, false)
 }, false)
