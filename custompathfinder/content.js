4d9340364702ce6fc1e4de0e0dd413d8fc0f7b95
Sai Punith Bonagiri <saipunithbonagiri@gmail.com>
2022-08-09 06:13:22 +0530

    Creation and Displaying of Tour

--- custompathfinder/content.js
+++ custompathfinder/content.js
@@ -1,70 +1,71 @@
-// configure theRoom
-window.theRoom.configure({
-  blockRedirection: true,
-  createInspector: true,
-  excludes: [],
-  click: function (element, event) {
-    debugger;
-    event.preventDefault()
-    event.stopPropagation();
-    // get the unique css selector of the clicked element
-    // and then copy it to clipboard
-    navigator
-      .clipboard
-      .writeText(
-        getSelector(element)
-      )
-      .then(
-        function () {
-          alert('The unique CSS selector successfully copied to clipboard')
-        },
-        function (err) {
-          alert('The unique CSS selector could not be copied to clipboard')
-        }
-      )
+(() => {// configure theRoom
+  window.theRoom.configure({
+    blockRedirection: true,
+    createInspector: true,
+    excludes: [],
+    click: function (element, event) {
+      debugger;
+      event.preventDefault()
+      event.stopPropagation();
+      // get the unique css selector of the clicked element
+      // and then copy it to clipboard
+      navigator
+        .clipboard
+        .writeText(
+          getSelector(element)
+        )
+        .then(
+          function () {
+            alert('The unique CSS selector successfully copied to clipboard')
+          },
+          function (err) {
+            alert('The unique CSS selector could not be copied to clipboard')
+          }
+        )
 
-    // so far so good
-    // stop inspection
-     window.theRoom.stop(true)
-  },
-  keydown: function (element, event) {
-    console.log("Inside keydown function in content.js");
-    console.log("Element Selected"+getSelector(element));
-    var swalTemplateElement=document.getElementById('SwalTemplate');
-    var swalTemplateContentCloned=swalTemplateElement.content.firstElementChild.cloneNode(true);
-    Swal.fire({
-        html:swalTemplateContentCloned,
+      // so far so good
+      // stop inspection
+      window.theRoom.stop(true)
+    },
+    keydown: function (element, event) {
+      console.log("Inside keydown function in content.js");
+      console.log("Element Selected" + getSelector(element));
+      var swalTemplateElement = document.getElementById('SwalTemplate');
+      var swalTemplateContentCloned = swalTemplateElement.content.firstElementChild.cloneNode(true);
+      Swal.fire({
+        html: swalTemplateContentCloned,
         showCloseButton: true,
         allowOutsideClick: false,
         allowEscapeKey: false,
         confirmButtonText: 'Save',
-        willOpen:function(element){
+        willOpen: function (element) {
           window.theRoom.stop(true);
-          var o9SplitViewElement=document.getElementById('__splitview__');
-          if(o9SplitViewElement){
-            o9SplitViewElement.style.position='fixed';
+          var o9SplitViewElement = document.getElementById('__splitview__');
+          if (o9SplitViewElement) {
+            o9SplitViewElement.style.position = 'fixed';
           }
         },
-        willClose:function(element){
-          var o9SplitViewElement=document.getElementById('__splitview__');
-          if(o9SplitViewElement){
-            o9SplitViewElement.style.position='';
+        willClose: function (element) {
+          var o9SplitViewElement = document.getElementById('__splitview__');
+          if (o9SplitViewElement) {
+            o9SplitViewElement.style.position = '';
           }
         },
-        preConfirm:function(){
+        preConfirm: function () {
           //To get the element's input values and storing them before swal closes by confirm button
         }
-    }).then(function(result){
-      window.theRoom.start();
-    });
+      }).then(function (result) {
+        window.theRoom.start();
+      });
 
-    // so far so good
-    // stop inspection
-    // window.theRoom.stop(true);
-  }
-})
+      // so far so good
+      // stop inspection
+      // window.theRoom.stop(true);
+    }
+  })
 
-var generateAndAppendTemplate = function () {
+  var generateAndAppendTemplate = function () {
+    //SwalTemplate Addition
     var swalTemplateElement = document.createElement('template');
     swalTemplateElement.setAttribute('id', "SwalTemplate");
     var MainDiv = document.createElement('div');
@@ -95,22 +96,46 @@ var generateAndAppendTemplate = function () {
     swalTemplateElement.content.appendChild(MainDiv);
 
     document.body.appendChild(swalTemplateElement);
-};
+  };
 
-// inspector element styles
-var linkElement = document.createElement('link');
-linkElement.setAttribute('rel', 'stylesheet');
-linkElement.setAttribute('type', 'text/css');
-linkElement.setAttribute('href', 'data:text/css;charset=UTF-8,' + encodeURIComponent('.inspector-element { position: absolute; pointer-events: none; border: 2px solid tomato; transition: all 200ms; background-color: rgba(180, 187, 105, 0.2); z-index:2147483644 }'));
-document.head.appendChild(linkElement);
+  var fetchData = function (currentHostName) {
+    return new Promise((resolve, reject) => {
+      chrome.storage.sync.get([currentHostName], (obj) => {
+        resolve(obj[currentHostName] ? JSON.parse(obj[currentHostName]) : []);
+      })
+    });
+  };
 
-//Creation of Modal Template Element and appending it to body
-generateAndAppendTemplate();
+  // inspector element styles
+  var linkElement = document.createElement('link');
+  linkElement.setAttribute('rel', 'stylesheet');
+  linkElement.setAttribute('type', 'text/css');
+  linkElement.setAttribute('href', 'data:text/css;charset=UTF-8,' + encodeURIComponent('.inspector-element { position: absolute; pointer-events: none; border: 2px solid tomato; transition: all 200ms; background-color: rgba(180, 187, 105, 0.2); z-index:2147483644 }'));
+  document.head.appendChild(linkElement);
 
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  // the expected message has arrived
-  // ready to start inspection
+  //Creation of Modal Template Element and appending it to body
+  generateAndAppendTemplate();
 
-  // inspection has started
-  window.theRoom.start()
-});
+  var sendResponseDoop= async function(object,sender,sendResponse){
+    var { type, payload } = object;
+    var tourHostName = payload.tourObj.tourHostName;
+    var allDataForHostName = await fetchData(tourHostName);
+    var newData = [...allDataForHostName, payload];
+    chrome.storage.sync.set({
+      [tourHostName]: JSON.stringify(newData)
+    });
+    sendResponse(newData);
+  };
+
+  chrome.runtime.onMessage.addListener(function (object, sender, sendResponse) {
+    // the expected message has arrived
+    // ready to start inspection
+    var { type, payload } = object;
+    if (type === "NEW" && payload?.tourObj?.tourHostName) {
+      sendResponseDoop(object,sender,sendResponse);
+      return true;
+    }
+    // inspection has started
+    window.theRoom.start()
+  });
+})();
--- custompathfinder/popup.js
+++ custompathfinder/popup.js
@@ -1,3 +1,19 @@
+//Utils Section Start
+var getCurrentTab= async ()=>{
+  let queryOptions={active:true,lastFocusedWindow:true};
+  /*The below is called destructuring syntax..(Read more about it here https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment)
+  An example:
+      const obj = { a: 1, b: 2 };
+      const { a, b } = obj;
+      // is equivalent to:
+      // const a = obj.a;
+      // const b = obj.b;
+  */
+  let [tab]=await chrome.tabs.query(queryOptions);
+  return tab;
+};
+//Utils Section End
+
 var toggleElementFunction = function () {
   $('.toggle').click(function (e) {
     e.preventDefault();
@@ -16,8 +32,61 @@ var toggleElementFunction = function () {
   });
 };
 
+var getRandomId=function(){
+  return crypto.randomUUID();
+}
 
-var initiateAllEventListeners=function(){
+var updateAccordionList=function(currentStorageData=[]){
+  console.log("CurrentStorageData in popup.js updateAccordionList method below: ");
+  console.log(currentStorageData.length);
+  console.log(currentStorageData);
+  var currentAccordionUlElement=document.querySelector('.AccordionList ul');
+  currentAccordionUlElement.innerHTML='';
+  //should auto generate the elements and append to accordion list and then show the accordion and hide the input form
+  var tourElementTemplate = document.getElementById('TourElementTemplate');
+
+  for(var i=0;i<currentStorageData.length;i++){
+      var defaultTourName='Tour #'+i;
+      var defaultTourDescription='Tour #'+i+" Description";
+      var currTourName=currentStorageData[i].tourObj?.tourName;
+      currTourName=(currTourName)?currTourName:defaultTourName;
+      var currTourDescription=currentStorageData[i].tourObj?.tourDescription;
+      currTourDescription=(currTourDescription)?currTourDescription:defaultTourDescription;
+      var liElement=document.createElement('li');
+      var tourElementCloned = tourElementTemplate.content.firstElementChild.cloneNode(true);
+      tourElementCloned.querySelector('a').innerHTML=currTourName;
+      tourElementCloned.querySelector('a').setAttribute('title',currTourDescription);
+      liElement.appendChild(tourElementCloned);
+
+      var steps=currentStorageData[i].tourObj?.steps;
+      for(var j=0;j<steps?.length;j++){
+        var defaultStepName='Step #' +j;
+        var defaultStepDescription='Step #'+i+'Description';
+        var currStepName=steps[j]?.stepName;
+        currStepName=(currStepName)?currStepName:defaultStepName;
+        var currStepDescription=steps[j]?.stepDescription;
+        currStepDescription=(currStepDescription)?currStepDescription:defaultStepDescription;
+        var ulElement=document.createElement('ul');
+        ulElement.setAttribute('class','inner');
+        var stepElementTemplate= document.getElementById('StepElementTemplate');
+        var stepElementCloned=stepElementTemplate.content.firstElementChild.cloneNode(true);
+        stepElementCloned.querySelector('.StepText').innerHTML=currStepName;
+        stepElementCloned.querySelector('.StepElement').setAttribute('title',currStepDescription);
+        ulElement.appendChild(stepElementCloned);
+        liElement.appendChild(ulElement);
+      }
+      currentAccordionUlElement.appendChild(liElement);
+  }
+  var accordionListElement=document.querySelector('.AccordionList');
+  var formInputElement=document.querySelector('.TourForm');
+  var createTourButton=document.querySelector('.CreateTour');
+  var createTourParent=createTourButton.parentElement;
+  accordionListElement.style.display='block';
+  createTourParent.style.display='block';
+  formInputElement.style.display='none';
+};
+
+var initiateAllEventListeners=async function(){
   toggleElementFunction();
   var accordionListElement=document.querySelector('.AccordionList');
   var formInputElement=document.querySelector('.TourForm');
@@ -26,28 +95,112 @@ var initiateAllEventListeners=function(){
   var createTourElementButton = document.querySelector('.CreateTour');
   var cancelButton=document.querySelector('.buttonGroupForm .cancelButton');
   var saveButton=document.querySelector('.buttonGroupForm .saveButton');
+  var tourNameElement=document.querySelector('.TourForm input');
+  var tourDescriptionElement=document.querySelector('.TourForm textarea');
   createTourElementButton.addEventListener('click',function(e){
     accordionListElement.style.display='none';
     createTourParent.style.display='none';
     formInputElement.style.display='flex';
   });
 
-  cancelButton.addEventListener('click',function(e){
+  cancelButton.addEventListener('click', function(e){
     accordionListElement.style.display='block';
     createTourParent.style.display='block';
     formInputElement.style.display='none';
   });
 
-  // saveButton.addEventListener('click',function(e){
-  //   //tour_id,tour_name,tour_description,tourUrl
-  // });
+  saveButton.addEventListener('click',async function(e){
+    //tourId,tourName,tourDescription,tourUrl,isActive,tourHostName
+    //Make the current tour as active in the storage, such that when we send the payload from site's ui we can identify to which object we need to append to
+    var tourName=tourNameElement.value;
+    var tourDescription=tourDescriptionElement.value;
+    var tourId=getRandomId();
+    var isActive=true;
+    const activeTab= await getCurrentTab();
+    var urlObject=new URL(activeTab.url);
+    //ToDo: To store this in hashed manner later. This will be the key of our tourObj
+    var tourHostName=urlObject.hostname;
+    //Storing new tour object in chrome storage by sending this info to ContentScript which adds and returns the updated info
+    var payload={
+      "tourId":tourId,
+      "isActive":isActive,
+      "tourObj":{
+        "tourName":tourName,
+        "tourDescription":tourDescription,
+        "tourUrl":activeTab.url,
+        "tourHostName":tourHostName,
+        "tenantId":'7586',
+        "tenantName":'shubhqa',
+        'steps':[]
+      }
+    };
+    chrome.tabs.sendMessage(activeTab.id,{
+      type:"NEW",
+      payload: payload
+    },updateAccordionList);
+  });
 }
+
+var generateAndAppendTemplate = function () {
+  //TourElement Template Addition
+  var tourElementTemplate = document.createElement('template');
+  tourElementTemplate.setAttribute('id', "TourElementTemplate");
+  var tourElementDiv = document.createElement('div');
+  tourElementDiv.setAttribute('class', 'TourElement');
+  var anchorElement = document.createElement('a');
+  //To add text during runtime for this elem
+  anchorElement.setAttribute('class', 'toggle');
+  anchorElement.setAttribute('href', '#');
+  var buttonGroup = document.createElement('div');
+  buttonGroup.setAttribute('class', 'buttonGroupOuter');
+  var editButtonElement0 = document.createElement('button');
+  var addButtonElement0 = document.createElement('button');
+  var saveButtonElement0 = document.createElement('button');
+  var deleteButtonElement0 = document.createElement('button');
+  editButtonElement0.innerHTML = 'E';
+  buttonGroup.appendChild(editButtonElement0);
+  addButtonElement0.innerHTML = 'A';
+  buttonGroup.appendChild(addButtonElement0);
+  saveButtonElement0.innerHTML = 'S';
+  buttonGroup.appendChild(saveButtonElement0);
+  deleteButtonElement0.innerHTML = 'D';
+  buttonGroup.appendChild(deleteButtonElement0);
+  tourElementDiv.appendChild(anchorElement);
+  tourElementDiv.appendChild(buttonGroup);
+  tourElementTemplate.content.appendChild(tourElementDiv);
+  document.body.appendChild(tourElementTemplate);
+
+  //StepElement Template Addition
+  var stepElementTemplate = document.createElement('template');
+  stepElementTemplate.setAttribute('id', "StepElementTemplate");
+  var listElement = document.createElement('li');
+  var stepElement = document.createElement('div');
+  stepElement.setAttribute('class', 'StepElement');
+  var stepTextElement = document.createElement('div');
+  stepTextElement.setAttribute('class', 'StepText');
+  var buttonGroupInnerElement = document.createElement('div');
+  buttonGroupInnerElement.setAttribute('class', 'buttonGroupInner');
+  var editButtonElement = document.createElement('button');
+  var saveButtonElement = document.createElement('button');
+  var deleteButtonElement = document.createElement('button');
+  editButtonElement.innerHTML = 'E';
+  buttonGroupInnerElement.appendChild(editButtonElement);
+  saveButtonElement.innerHTML = 'S';
+  buttonGroupInnerElement.appendChild(saveButtonElement);
+  deleteButtonElement.innerHTML = 'D';
+  buttonGroupInnerElement.appendChild(deleteButtonElement);
+  stepElement.appendChild(stepTextElement);
+  stepElement.appendChild(buttonGroupInnerElement);
+  listElement.appendChild(stepElement);
+  stepElementTemplate.content.appendChild(listElement);
+  document.body.appendChild(stepElementTemplate);
+};
 
 document.addEventListener('DOMContentLoaded', function () {
   // get the cta button element
   initiateAllEventListeners();
   
-
+  generateAndAppendTemplate();
   // handle cta button click event
   // to be able to start inspection
   // selectElementButton.addEventListener('click', function () {
